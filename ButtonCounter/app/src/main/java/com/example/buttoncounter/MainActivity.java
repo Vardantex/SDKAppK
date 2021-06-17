eb39c237a6745d0fd0640187d8dd5d1d02534c04
Andrew G <vardantexcode@gmail.com>
2021-06-16 20:38:03 -0400

    Optimized edited text

--- ButtonCounter/app/src/main/java/com/example/buttoncounter/MainActivity.java
+++ ButtonCounter/app/src/main/java/com/example/buttoncounter/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.buttoncounter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -9,12 +7,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
     private TextView textView;
-    private Button button;
-    private int numTimesClicked = 0;
 
 
     @Override
@@ -24,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         //Implement the id from activity main
         userInput = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+
         //Setting a blank text resets any current text left by the onclick listener
         textView.setText("");
         //Movement method needs the veritcal scrolling added in the layout
@@ -35,11 +34,14 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numTimesClicked = numTimesClicked + 1;
-                String result = "\n Button pressed " + numTimesClicked + " times!";
-                //Append textview to result for output
-                //Append Adds to the current test
+                //Create a result of the user input to string
+                String result =  userInput.getText().toString();
+                result = result +"\n";
+                //Add append textview to result to get output
                 textView.append(result);
+                userInput.setText("");
+
+
             }
         }; // End of onclick
 
